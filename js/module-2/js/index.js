ba95a3d73a887d72375a9e64d1a80f772588ca30
SvisT <svisteckij@gmail.com>
2019-05-19 18:14:17 +0300

    some fixes

--- js/module-2/js/index.js
+++ js/module-2/js/index.js
@@ -6,32 +6,36 @@ while (true) {
     input = prompt('Enter number!');
     if  (input === null) {
         break;
-    } numbers.push(input);
-}
-
-if (numbers.length > 0) {
-    for (number of numbers) {
-        total = total + number;
+    } else if (isNaN(Number(input))) {
+        continue;
+    } else {
+        numbers.push(Number(input));
     }
 }
 
-console.log(`Общая сумма чисел равна ${total}`);
+if (numbers.length) {
+    for (number of numbers) {
+        total = total + number;
+    }
+    console.log(`Общая сумма чисел равна ${total}`);
+}
+
 
 const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
 let attemptsLeft = 3;
 
-while (attemptsLeft > 0) {
+while (attemptsLeft) {
     let userPassword = prompt('Enter password!');
-    if (passwords.includes(userPassword)) {
+    if (userPassword === null) {
+        break;
+    } else if (passwords.includes(userPassword)) {
         alert('Добро пожаловать!');
         break;
-    } else if (userPassword !== null && attemptsLeft > 0) {
+    } else if (userPassword !== null && attemptsLeft) {
         attemptsLeft -= 1;
-        if (attemptsLeft === 0) {
+        if (!attemptsLeft) {
             alert('У вас закончились попытки, аккаунт заблокирован!');
             break;
         } alert(`Неверный пароль, у вас осталось ${attemptsLeft} попыток`);
-    } else if (userPassword === null) {
-        break;
     }
 }
