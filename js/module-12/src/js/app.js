0484be3172a9079a1f8a4e4b1b874267a8289461
SvisT <svisteckij@gmail.com>
2019-08-29 16:15:27 +0300

    homework 12 done

---
+++ js/module-12/src/js/app.js
@@ -0,0 +1,64 @@
+'use strict';
+
+import MicroModal from 'micromodal';
+import { Notyf } from 'notyf';
+import 'notyf/notyf.min.css';
+import listItemsTemplate from '../templates/listItems.hbs'
+import { PRIORITY_TYPES, ICON_TYPES, NOTE_ACTIONS, NOTIFICATION_MESSAGES } from './utils/constants';
+import { listRef, renderNoteList } from './utils/view';
+import Notepad from './utils/notepad-model';
+import initialNotes from '../assets/notes.json'
+const shortid = require('shortid');
+const notepad = new Notepad(initialNotes);
+const notyf = new Notyf();
+
+let notesFromLS = localStorage.getItem('notes');
+export let parsedItem = JSON.parse(notesFromLS);
+
+const addListItem = event => {
+  event.preventDefault();
+  const [title, body] = event.target.elements;
+  if (title.value.trim() === '' || body.value.trim() === '') {
+    return notyf.error(NOTIFICATION_MESSAGES.EDITOR_FIELDS_EMPTY);
+  }
+  const listItem = {
+    id: shortid.generate(),
+    title: title.value,
+    body: body.value,
+    priority: PRIORITY_TYPES.LOW
+  };
+  notepad.saveNote(listItem).then(addedItem => renderNoteList(listRef, addedItem));
+  MicroModal.close('note-editor-modal');
+  listRef.form.reset();
+  notyf.success(NOTIFICATION_MESSAGES.NOTE_ADDED_SUCCESS);
+}
+
+const deleteListItem = event => {
+  const action = event.target.parentElement.dataset.action;
+  if (action !== NOTE_ACTIONS.DELETE) {
+    return;
+  }
+  const parentListItem = event.target.closest('.note-list__item');
+  const id = parentListItem.dataset.id;
+  notepad.deleteNote(id);
+  parentListItem.remove();
+  notyf.success(NOTIFICATION_MESSAGES.NOTE_DELETED_SUCCESS);
+}
+
+const filterNotes = event => {
+  const filtredItem = notepad.filterNotesByQuery(event.target.value);
+  renderNoteList(listRef, filtredItem);
+}
+
+const openMicroModal = () => {
+  MicroModal.show('note-editor-modal');
+}
+
+
+console.log('Все текущие заметки: ', notepad.notes);
+renderNoteList(listRef, parsedItem);
+
+listRef.form.addEventListener('submit', addListItem);
+listRef.noteList.addEventListener('click', deleteListItem);
+listRef.searchForm.addEventListener('input', filterNotes);
+listRef.openEditorBtn.addEventListener('click', openMicroModal);
---
+++ js/module-12/src/js/utils/notepad-model.js
@@ -0,0 +1,69 @@
+import { prototype } from "events";
+import { parsedItem } from "../app";
+import { listRef, renderNoteList } from './view';
+
+export default class Notepad {
+    constructor(
+        initialNotes
+    ) {
+        this._notes = initialNotes;
+    }
+
+    get notes() {
+        return this._notes;
+    }
+    findNoteById(id) {
+        for (const note of this.notes) {
+            if (note.id === id) {
+                return note;
+            }
+        }
+    };
+    saveNote(note) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                this.notes.push(note);
+                localStorage.setItem('notes', JSON.stringify(this.notes));
+                resolve(this.notes)
+            }, 200);
+        })
+    };
+    deleteNote(id) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                this._notes = this._notes.filter(note => note.id !== id);
+                localStorage.setItem('notes', JSON.stringify(this.notes));
+                resolve(this.notes)
+            }, 201);
+        })
+    };
+    updateNoteContent(id, updatedContent) {
+        const note = this.findNoteById(id);
+        if (!note) return;
+        Object.assign(note, updatedContent);
+        return note;
+    };
+    updateNotePriority(id, priority) {
+        return this.findNoteById(id).priority = priority;
+    };
+    filterNotesByQuery(query) {
+        const newNotes = [];
+        for (let note of this.notes) {
+            const wordToLow = query.toLowerCase();
+            const titleToLow = note.title.toLowerCase();
+            const bodyToLow = note.body.toLowerCase();
+            if (titleToLow.includes(wordToLow) || bodyToLow.includes(wordToLow)) {
+                newNotes.push(note);
+            }
+        }
+        return newNotes;
+    };
+    filterNotesByPriority(priority) {
+        const newNotes = [];
+        for (let note of this.notes) {
+            if (note.priority === priority)
+                newNotes.push(note);
+        }
+        return newNotes;
+    };
+};  
